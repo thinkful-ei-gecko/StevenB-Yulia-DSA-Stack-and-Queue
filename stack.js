fd39f3c7e4622a41aa515a7fe51c640c05d8ec0a
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-08 08:35:24 -0800

    create stack and useful methods

---
+++ stack.js
@@ -0,0 +1,84 @@
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    /* If the stack is empty, then the node will be the
+       top of the stack */
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+
+    /* If the stack already has something, 
+       then create a new node,
+       add data to the new node, and
+       have the pointer point to the top */
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+
+  pop() {
+    /* In order to remove the top of the stack, you have to point
+       the pointer to the next item and that next item becomes the
+       top of the stack */
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+}
+
+function peek(stack) {
+  if (stack.top === null) {
+    console.log('Stack is empty');
+    return;
+  }
+  return stack.top;
+}
+
+function isEmpty(stack) {
+  if (stack.top === null) {
+    return true;
+  }
+  return false;
+}
+
+function display(stack) {
+  if (stack.top === null) {
+    console.log('Stack is empty');
+    return;
+  }
+  let currNode = stack.top;
+  while(currNode.next !== null) {
+    console.log(currNode.data);
+    currNode = currNode.next;
+  }
+  return currNode.data;
+}
+
+function main() {
+  const starTrek = new Stack();
+
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+
+  // peek(starTrek);
+  // isEmpty(starTrek);
+  starTrek.pop();
+  starTrek.pop();
+
+  return display(starTrek);
+}
+// console.log(JSON.stringify(main()));
+console.log(main());
+
